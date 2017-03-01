9516e18b40d8958ed98eb9a654a7d6cf6e202704
vlatko <vlatko@mono.hr>
2017-03-01 17:28:44 +0100

    exported extension interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './IBaasicApp';
 export * from './diModule';
 export * from './baasicApp';
+export { IHttpClient, IHttpHeaders, IHttpRequest, IHttpResponse } from './httpApi';
+export { IEventHandler, ITokenHandler  } from "./core/contracts";
 
