12ab616af433fb89ace6e5cbec4f1fc2aa47b099
Kristijan Horvat <kristijan@mono-software.com>
2018-05-21 09:56:04 +0200

    The bug related to user registration missing DisplayName, FirstName and LastName fields is fixed

--- src/modules/membership/contracts/IRegisterUser.ts
+++ src/modules/membership/contracts/IRegisterUser.ts
@@ -3,6 +3,9 @@ import { IUserIdentity } from './';
 
 export interface IRegisterUser extends IModel, IUserIdentity {
     activationUrl?: string,
+    displayName: string,
+    firstName?: string,
+    lastName?: string,
     challengeIdentifier?: string,
     challengeResponse?: string,
     confirmPassword: string,
