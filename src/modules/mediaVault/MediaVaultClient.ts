3000058edd10e347ff6bef1682436d145ff4c4f0
Helena Stjepanović <hstjepanovic@mono-software.com>
2018-06-07 11:16:02 +0200

    added missing parameter on media gallery file route, fixed update route parameter on media vault client

--- src/modules/mediaVault/MediaVaultClient.ts
+++ src/modules/mediaVault/MediaVaultClient.ts
@@ -110,7 +110,7 @@ export class MediaVaultClient {
                         }); 				
      **/
     update(data: IMediaEntry): PromiseLike<IHttpResponse<void>> {
-        return this.apiClient.put<void>(this.routeDefinition.updateParams(data), this.routeDefinition.updateParams(data));
+        return this.apiClient.put<void>(this.routeDefinition.update(data), this.routeDefinition.updateParams(data));
     }
 
     /**                  
